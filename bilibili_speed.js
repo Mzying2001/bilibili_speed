2ac7d7a05d06697e69e043bd182518e579b25910
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-01-06 13:58:22 +0800

    Create bilibili_speed.js

---
+++ bilibili_speed.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         哔哩哔哩倍速
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-06
+// @description  try to take over the world!
+// @author       Mzying2001
+// @match        http*://www.bilibili.com/video/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    var intervalId = setInterval(function () {
+        var ulElement = document.querySelector('.bpx-player-ctrl-playbackrate-menu');
+        if (ulElement) {
+            var newLiHTML = '<li class="bpx-player-ctrl-playbackrate-menu-item" data-value="4">4.0x</li>' +
+                '<li class="bpx-player-ctrl-playbackrate-menu-item" data-value="3">3.0x</li>';
+            ulElement.insertAdjacentHTML('afterbegin', newLiHTML);
+            clearInterval(intervalId);
+            console.log('done');
+        }
+    }, 1000)
+})()
